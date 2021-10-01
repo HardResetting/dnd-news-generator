4fe6232595952a022af1b71234af873e7d0823d9
HardResetting <sribnij_d@web.de>
2021-10-01 20:19:31 +0200

    Added Bootstrap 5.1

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,19 +1,23 @@
-<!DOCTYPE html>
-<html lang="de">
-
-<head>
+<!doctype html>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ env('APP_NAME') }}</title>
-
-    <link href="css/main.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/0c00d2bac9.js" crossorigin="anonymous"></script>
     <script src="js/script.js" defer></script>
-</head>
 
-<body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>{{ env('APP_NAME') }}!</title>
+  </head>
+  <body>
+    <h1>{{ env('APP_NAME') }}!</h1>
+
     @yield('content')
-</body>
 
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  </body>
 </html>
