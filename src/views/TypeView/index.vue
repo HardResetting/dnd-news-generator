68ca5494a557630299874fc18680265912b03937
HardResetting <sribnij_d@web.de>
2021-11-10 09:03:15 +0100

    Table now Sortable

--- src/views/TypeView/index.vue
+++ src/views/TypeView/index.vue
@@ -1,3 +1,10 @@
+<style scoped>
+.sortable {
+  cursor: pointer !important;
+}
+</style>
+
+
 <template>
   <div>
     <div class="d-flex flex-row">
@@ -8,10 +15,22 @@
             <div>
               <div class="form-group">
                 <label for="name">value</label>
-                <input class="form-control" v-bind:class="{ 'is-invalid': isInvalid }" v-model.trim="newType" />
-                <div class="invalid-feedback">Only letters and underscores are allowed!</div>
+                <input
+                  class="form-control"
+                  v-bind:class="{ 'is-invalid': isInvalid }"
+                  v-model.trim="newType"
+                />
+                <div class="invalid-feedback">
+                  Only letters and underscores are allowed!
+                </div>
               </div>
-              <button class="btn btn-primary mt-2" v-bind:class="{ 'disabled': isInvalid }" @click="addType">Add</button>
+              <button
+                class="btn btn-primary mt-2"
+                v-bind:class="{ disabled: isInvalid }"
+                @click="addType"
+              >
+                Add
+              </button>
             </div>
           </div>
         </div>
@@ -24,13 +43,13 @@
         <table class="table table-striped">
           <thead>
             <tr>
-              <th scope="col">Value</th>
+              <th class="sortable" scope="col" @click="sort('value')">Value</th>
               <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
             <TypesTabledata
-              v-for="type in types"
+              v-for="type in sortedTypes"
               :key="type.key"
               :type="type"
             ></TypesTabledata>
@@ -41,13 +60,18 @@
   </div>
 </template>
 
-
 <script lang="ts">
 import { defineComponent } from "vue";
 import { db } from "../../services/FirestoreDb";
 import { IPlainObject } from "../../../typings/Globals";
 import TypesTabledata from "./TypesTabledata.vue";
-import { doc, collection, getDocs, setDoc } from "firebase/firestore";
+import { doc, collection, getDocs, addDoc } from "firebase/firestore";
+
+// credit: Typescript documentation, src
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#index-types
+function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
+  return o[propertyName]; // o[propertyName] is of type T[K]
+}
 
 const Component = defineComponent({
   name: "types",
@@ -60,17 +84,39 @@ const Component = defineComponent({
     return {
       types: new Array<IPlainObject>(),
       newType: "",
+      currentSort: "value",
+      currentSortDir: "asc",
+      isLoading: true,
     };
   },
 
   methods: {
     addType() {
-      console.log(this.newType);
+      if (!this.isInvalid) {
+        addDoc(collection(db, "types"), {
+          value: this.newType,
+          items: [],
+        }).then(
+          (value) => {
+            this.types.push({ key: value.id, value: this.newType });
+            this.newType = "";
+          },
 
-      // setDoc(doc(db, "types"), {
-      //   value: this.newType,
-      //   items: [],
-      //   });
+          (reason) => {
+            console.log(`Write Failed! Reason: ${reason}`);
+          }
+        );
+      }
+    },
+
+    sort(s: string) {
+      //if s == current sort, reverse
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+      } else {
+        this.currentSort = s;
+        this.currentSortDir = "asc";
+      }
     },
   },
 
@@ -79,20 +125,35 @@ const Component = defineComponent({
       console.log(querySnapshot);
 
       querySnapshot.forEach((doc: any) => {
-        console.log(doc);
-        console.log(doc.data());
-
         this.types.push({
           key: doc.id,
           value: doc.data().value,
         });
       });
+      this.isLoading = false;
     });
   },
 
   computed: {
     isInvalid(): boolean {
       return /[^A-Za-z_]/.test(this.newType);
+    },
+    sortedTypes(): IPlainObject[] {
+      if(this.isLoading) return this.types;
+
+      return [...this.types].sort((a: any, b: any) => {
+        let modifier = this.currentSortDir === "asc" ? 1 : -1;
+
+        var cur = a[this.currentSort];
+        var next = b[this.currentSort];
+
+        console.log(cur);
+        
+        return (
+          cur.localeCompare(next, undefined, { sensitivity: "accent" }) *
+          modifier
+        );
+      });
     },
   },
 });
