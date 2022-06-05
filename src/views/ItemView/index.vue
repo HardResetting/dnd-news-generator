7c3d1c998af489a6d4bbbef754c2649a3100ac66
HardResetting <52751216+HardResetting@users.noreply.github.com>
2022-06-05 22:07:00 +0200

    Revert "Debughelp"
    This reverts commit 1ad341d205ff7194cad289aa4b769aa34d2da99e.

--- src/views/ItemView/index.vue
+++ src/views/ItemView/index.vue
@@ -245,7 +245,6 @@ async function addType(): Promise<void> {
     newItem.value.plural,
     newItem.value.type
   );
-  console.dir(templateItem);
   state.DATABASE_ADD_FIREBASE_TEMPLATE_ITEM(templateItem);
   resetForm();
 }
