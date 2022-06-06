cc7f523c67a2a8081e09b4247784c6b838d56f52
HardResetting <52751216+HardResetting@users.noreply.github.com>
2022-06-06 13:16:18 +0200

    WIP Pagination + Making Table generic component

---
+++ src/components/SimpleTable.vue
@@ -0,0 +1,332 @@
+<script lang="ts" setup>
+export type Header = {
+    name: string,
+    text: string,
+    searchable: boolean,
+    sortable: boolean,
+    onItemClick?: (item: Record<string, any>) => void,
+}
+
+export type Item = {
+    // key value pair array
+    data: ComputedRef<Array<Record<string, any>>>,
+    onEditClick?: (item: Record<string, any>) => void,
+    onDeleteClick?: (item: Record<string, any>) => void,
+}
+
+import { computed, ref, type ComputedRef, type PropType, type Ref } from 'vue';
+import BasicCard from './BasicCard.vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'NO_TITLE',
+    },
+    headers: {
+        type: Array as PropType<Array<Header>>,
+        required: true,
+    },
+    items: {
+        type: Object as PropType<Item>,
+        required: true,
+    },
+    filterType: {
+        type: String,
+        default: '',
+    },
+    reducedPadding: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const anyActionDefined = props.items.onEditClick != undefined || props.items.onDeleteClick != undefined;
+
+const currentSortValue = ref('');
+const currentSortDirection: Ref<'asc' | 'desc'> = ref('asc');
+
+const searchbarValue = ref('');
+
+const sortedFilteredItems = computed(() => {
+    const sortValue = currentSortValue.value;
+    const sortDir = currentSortDirection.value;
+
+    const sortedArray = sortValue === ""
+        ? [...props.items.data.value]
+        : [...props.items.data.value]
+            .sort((a: Record<string, string>, b: Record<string, string>) =>
+                a[sortValue].toString().localeCompare(b[sortValue].toString()) * (sortDir === 'asc' ? 1 : -1)
+            )
+
+    const filteredArray = [...props.items.data.value].filter((item: Record<string, string>) => {
+
+        const ok = props.headers.some((header: Header) => {
+            if (!header.searchable) return false;
+
+            return (item[header.name].toString().toLowerCase().includes(searchbarValue.value.toLowerCase()))
+        });
+
+        return ok;
+    });
+
+    return filteredArray;
+});
+
+const paginationBreakPoint = 10
+const currentPage = ref(0);
+const maxPage = computed(() =>
+    sortedFilteredItems.value.length > 0
+        ? Math.ceil(sortedFilteredItems.value.length / paginationBreakPoint)
+        : 1
+);
+
+const sortedFilteredPaginatedItems = computed(() => {
+    if (sortedFilteredItems.value.length == 0) return sortedFilteredItems.value;
+
+    const start = currentPage.value * paginationBreakPoint;
+    const end = start + paginationBreakPoint;
+    return sortedFilteredItems.value.slice(start, end);
+});
+
+
+function sort(header: Header) {
+    if (!header.sortable)
+        return '';
+
+    currentSortValue.value = header.name;
+}
+
+function sortArrowClass(header: Header): string {
+    return currentSortValue.value === header.name
+        ? currentSortDirection.value === "asc"
+            ? "sort-arrow-asc"
+            : "sort-arrow-desc"
+        : "";
+}
+</script>
+<template>
+    <BasicCard :bodyPadding="false">
+        <template #title>
+            <slot name="title">
+                <h2>{{ title }}</h2>
+            </slot>
+        </template>
+        <template #title-side>
+            <div class="searchbar">
+                <label for="searchInput">Search:</label>
+                <input id="searchInput" placeholder="Type here..." v-model="searchbarValue" type="text"
+                    autocomplete="off" />
+                <div class="counter" style="flex-shrink: 0">
+                    ( {{ sortedFilteredItems.length }} of {{ items.data.value.length }} )
+                </div>
+            </div>
+        </template>
+        <template #body>
+            <table :class="{ 'reduced-padding': reducedPadding }" style="width: 100%">
+                <thead>
+                    <tr>
+                        <th v-for="header in headers" :key="header.name" class="sortable"
+                            :class="sortArrowClass(header)" scope="col" @click="sort(header)">
+                            {{ header.text }}
+                        </th>
+                        <th v-if="anyActionDefined" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-if="!sortedFilteredItems.length">
+                        <td colspan="4" class="noElements">No elements found!</td>
+                    </tr>
+                    <tr v-for="item in sortedFilteredPaginatedItems" :key="item.key">
+                        <td v-for="header in headers" :class="{ clickable: header.onItemClick != undefined }"
+                            @click.prevent.stop="header.onItemClick?.(item)">{{
+                                    item[header.name]
+                            }}</td>
+                        <td v-if="anyActionDefined" class="table-action">
+                            <button v-if="items.onEditClick != undefined" class="primary"
+                                @click="items.onEditClick?.(item)">
+                                Edit
+                            </button>
+                            <button v-if="items.onDeleteClick != undefined" class="danger"
+                                @click="items.onDeleteClick?.(item)">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="pagination">
+                <button class="secondary" @click.prevent="currentPage--" :disabled="currentPage === 0">
+                    <div class="arrow arrow-left"></div>
+                </button>
+                <button v-for="index in maxPage" class="secondary" :class="{ selected: index - 1 === currentPage }"
+                    @click="currentPage = index - 1">{{
+                            index
+                    }}</button>
+                <!-- <span class="secondary seperator">&hellip;</span>
+            <button class="secondary">15</button> -->
+                <button class="secondary" @click.prevent="currentPage++" :disabled="currentPage === maxPage - 1">
+                    <div class="arrow arrow-right"></div>
+                </button>
+            </div>
+        </template>
+    </BasicCard>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/colors.scss";
+$table-background-color: $contrast-background;
+$table-hover-background-color: darken($table-background-color, 8%);
+$table-even-background-color: darken($table-background-color, 4%);
+$table-header-color: $white;
+$table-header-background-color: #242e37;
+$table-border-color: #141625;
+$pagination-background-color: black;
+$pagination-label-color: $white;
+$pagination-label-background-color: $pagination-background-color;
+$pagination-label-active-color: $white;
+$pagination-label-active-background-color: darken($pagination-background-color, 15%);
+$pagination-label-disabled-color: $white;
+$pagination-label-disabled-background-color: lighten($pagination-background-color, 15%);
+$text-color: $white;
+
+.table-radio {
+    display: none;
+
+    &:checked+.card-title-side {
+        display: block;
+    }
+
+    &:checked+.card-title-side+table {
+        width: 100%;
+        display: table;
+    }
+
+    &:checked+.card-title-side+table+.pagination {
+        display: flex;
+
+        &>label.active {
+            color: $pagination-label-active-color;
+            background-color: $pagination-label-active-background-color;
+            cursor: default;
+        }
+
+        &>label.disabled {
+            color: $pagination-label-disabled-color;
+            background-color: $pagination-label-disabled-background-color;
+            cursor: default;
+        }
+    }
+}
+
+table {
+
+    &.reduced-padding tr th,
+    &.reduced-padding tr td {
+        padding: 0.5rem;
+    }
+
+    background-color: $table-background-color;
+    font-size: 16px;
+    border-collapse: collapse;
+    display: table;
+
+    tr {
+        &:last-child {
+            td {
+                border-bottom: 0;
+            }
+        }
+
+        th,
+        td {
+            text-align: left;
+            padding: 15px;
+            box-sizing: border-box;
+
+        }
+
+        th {
+            color: $table-header-color;
+            font-weight: normal;
+            background-color: $table-header-background-color;
+            border-bottom: solid 2px $table-border-color;
+            position: sticky;
+            top: 0;
+
+            &.table-action {
+                width: 1px;
+                white-space: nowrap;
+            }
+
+            &.sort-arrow-asc::after {
+                content: " ↑";
+            }
+
+            &.sort-arrow-desc::after {
+                content: " ↓";
+            }
+        }
+
+        td {
+            border-bottom: solid 1px $dark-white;
+
+            &.noElements {
+                text-align: center;
+            }
+
+            &.table-action {
+                text-align: right !important;
+            }
+        }
+    }
+
+    tbody tr {
+        transition: background-color 150ms ease-out;
+
+        &:nth-child(2n) {
+            background-color: $table-even-background-color;
+        }
+
+        &:hover {
+            background-color: $table-hover-background-color;
+        }
+    }
+}
+
+.pagination {
+    background-color: $pagination-background-color;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    padding: 5px 0;
+    background-color: $table-header-background-color;
+
+    .seperator {
+        margin-bottom: 0;
+        font-size: 150%;
+        display: inline-block;
+    }
+
+    button {
+        .arrow {
+            display: inline-block;
+            width: 8px;
+            height: 8px;
+            border-top: 2px solid $white;
+            border-left: 2px solid $white;
+
+            &.arrow-left {
+                transform: rotate(-45deg);
+            }
+
+            &.arrow-right {
+                transform: rotate(135deg);
+            }
+        }
+
+        margin-right: 0.25rem;
+        margin-left: 0.25rem;
+    }
+}
+</style>
--- src/views/ItemView/index.vue
+++ src/views/ItemView/index.vue
@@ -96,61 +96,8 @@
     </div>
 
     <div style="margin-top: 3rem">
-      <BasicCard :bodyPadding="false">
-        <template #title>
-          <h2>Items</h2>
-        </template>
-        <template #title-side>
-          <div class="searchbar">
-            <label for="searchInput">Search:</label>
-            <input id="searchInput" v-model="searchbarValue" placeholder="Type here..." type="text"
-              autocomplete="off" />
-            <div class="counter" style="flex-shrink: 0">
-              ({{ sortedFilteredItems.length }} of {{ state.FirebaseTemplateItems.length }})
-            </div>
-          </div>
-        </template>
-        <template #body>
-          <table style="width: 100%">
-            <thead>
-              <tr>
-                <th class="sortable" :class="sortArrowClass('singular')" scope="col" @click="sort('singular')">
-                  Singular
-                </th>
-                <th class="sortable" :class="sortArrowClass('plural')" scope="col" @click="sort('plural')">
-                  Plural
-                </th>
-                <th class="sortable" :class="sortArrowClass('type')" scope="col" @click="sort('type')">
-                  Types
-                </th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-if="!sortedFilteredItems.length">
-                <td colspan="4" class="noElements">No elements found!</td>
-              </tr>
-              <tr v-for="item in sortedFilteredItems" :key="item.key">
-                <td>{{ item.singular }}</td>
-                <td>{{ item.plural }}</td>
-                <td class="clickable">
-                  <router-link style="text-decoration: none;" :to="{ name: 'items', query: { type: item.type } }">{{
-                      item.type
-                  }}</router-link>
-                </td>
-                <td class="table-action">
-                  <button class="primary" @click="editItemPrompt(item.key)">
-                    Edit
-                  </button>
-                  <button class="danger" @click="deleteItemPrompt(item.key)">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </template>
-      </BasicCard>
+      <simple-table :items="items" :headers="headers" title="Items" :reducedPadding="true">
+      </simple-table>
     </div>
 
     <!-- Modals -->
@@ -181,29 +128,22 @@
 
 <script setup lang="ts">
 import { useVuelidate } from "@vuelidate/core";
-import { computed, ref, type Ref } from "vue";
+import { computed, onMounted, ref, type Ref } from "vue";
 import { FirebaseTemplateItem, TemplateItem } from "../../typings/Globals";
 import InputValidate from "../../components/InputValidate.vue";
 import InputValidateWithDatalist from "../../components/InputValidateWithDatalist.vue";
+import SimpleTable from "../../components/SimpleTable.vue";
 import BasicCard from "../../components/BasicCard.vue";
 import YesNoModal from "../../components/YesNoModal.vue";
 import { useStore } from "@/stores";
 import router from "@/router";
+import type { Item, Header } from "@/components/SimpleTable.vue.__VLS_script";
+
 
 const v$ = useVuelidate();
 const state = useStore();
-const props = defineProps({
-  type: {
-    type: String,
-    default: "",
-  },
-});
-
 
 const newItem: Ref<TemplateItem> = ref(new TemplateItem());
-const currentSortDir: Ref<"asc" | "desc"> = ref("asc");
-const currentSortValue: Ref<"singular" | "plural" | "type"> = ref("singular");
-const searchbarValue = ref("");
 const keepTypeValue: Ref<boolean> = ref(false);
 const singularSameAsPlural: Ref<boolean> = ref(false);
 const showDeleteModal: Ref<boolean> = ref(false);
@@ -211,6 +151,54 @@ const showEditModal: Ref<boolean> = ref(false);
 const selectedKey: Ref<string> = ref("");
 const selectedItem: Ref<FirebaseTemplateItem | undefined> = ref(undefined);
 const replacementItem: Ref<FirebaseTemplateItem | undefined> = ref(undefined);
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: "",
+  },
+});
+
+const items: Item = {
+  data: computed(() => state.FirebaseTemplateItems),
+  onEditClick: function (item: Record<string, any>): void {
+    const key = (item as any as FirebaseTemplateItem).key;
+
+    selectedKey.value = key;
+    selectedItem.value = state.getFirebaseTemplateItem(key);
+    replacementItem.value = new FirebaseTemplateItem(selectedItem.value!.key, selectedItem.value!.singular, selectedItem.value!.plural, selectedItem.value!.type);
+    toggleEditModal(true);
+  },
+  onDeleteClick: function (item: Record<string, any>): void {
+    const key = (item as any as FirebaseTemplateItem).key;
+
+    selectedKey.value = key;
+    toggleDeleteModal(false);
+    showDeleteModal.value = true;
+  }
+};
+
+const headers: Array<Header> = [
+  {
+    name: "singular",
+    text: "Singular",
+    searchable: true,
+    sortable: true,
+  },
+  {
+    name: "plural",
+    text: "Plural",
+    searchable: true,
+    sortable: true,
+  },
+  {
+    name: "type",
+    text: "Type",
+    searchable: true,
+    sortable: true,
+    onItemClick: (item) => goToItemsWithFilter((item as any as FirebaseTemplateItem).type)
+  },
+];
 
 function labelMessage(value: string) {
   return `Change from ${value} to:`;
@@ -224,12 +212,6 @@ function toggleEditModal(show: boolean) {
   showEditModal.value = show;
 }
 
-function editItemPrompt(key: string): void {
-  selectedKey.value = key;
-  selectedItem.value = state.getFirebaseTemplateItem(key);
-  replacementItem.value = new FirebaseTemplateItem(selectedItem.value!.key, selectedItem.value!.singular, selectedItem.value!.plural, selectedItem.value!.type);
-  toggleEditModal(true);
-}
 
 async function addType(): Promise<void> {
   const isValid: boolean = await v$.value.$validate();
@@ -250,18 +232,13 @@ async function addType(): Promise<void> {
 }
 
 function goToItemsWithFilter(s?: string): void {
-  if(typeof(s) === "undefined") {
+  if (typeof (s) === "undefined") {
     router.push({ name: "items" });
   } else {
-    router.push({ name: "items", query: { filter: s } });
+    router.push({ name: "items", query: { type: s } });
   }
 }
 
-function deleteItemPrompt(key: string): void {
-  selectedKey.value = key;
-  toggleDeleteModal(false);
-  showDeleteModal.value = true;
-}
 
 function editSelectedKey(): void {
   console.dir(newItem.value);
@@ -283,15 +260,6 @@ function resetForm(): void {
   v$.value.$reset();
 }
 
-function sort(s: "singular" | "plural" | "type") {
-  if (currentSortValue.value === s) {
-    currentSortDir.value = currentSortDir.value === "asc" ? "desc" : "asc";
-  } else {
-    currentSortValue.value = s;
-    currentSortDir.value = "asc";
-  }
-}
-
 const elementToText = computed(() => {
   const item: FirebaseTemplateItem | undefined =
     state.FirebaseTemplateItems.find((e) => e.key == selectedKey.value);
@@ -299,49 +267,4 @@ const elementToText = computed(() => {
     ? `${item.singular} / ${item.plural} of type "${item.type}"`
     : "Could not find element!";
 });
-
-const sortedFilteredItems = computed(() => {
-  const sortValue = currentSortValue.value;
-  const sortDir = currentSortDir.value;
-
-
-  const sortedArray = [...state.FirebaseTemplateItems]
-    .filter(item => {
-      const searchValue = searchbarValue.value.toLowerCase();
-      return (props.type == "" || item.type == props.type) &&
-        (
-          item.singular
-            .toLowerCase()
-            .includes(searchValue)
-          || item.plural
-            .toLowerCase()
-            .includes(searchValue)
-          || item.key
-            .toLowerCase()
-            .includes(searchValue)
-        )
-    })
-    .sort((a: FirebaseTemplateItem, b: FirebaseTemplateItem) => {
-      switch (sortValue) {
-        case "singular":
-          return a.singular.localeCompare(b.singular);
-        case "plural":
-          return a.plural.localeCompare(b.plural);
-        case "type":
-          return a.type.localeCompare(b.type);
-      }
-    })
-  if (sortDir === "desc") {
-    sortedArray.reverse();
-  }
-  return sortedArray;
-});
-
-function sortArrowClass(sortValue: "singular" | "plural" | "type"): string {
-  return currentSortValue.value === sortValue
-    ? currentSortDir.value === "asc"
-      ? "sort-arrow-asc"
-      : "sort-arrow-desc"
-    : "";
-}
 </script>
